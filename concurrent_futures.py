e68c2f326b2d9b3241988429951c65d14ee4a6ab
Waqas Obeidy <waqas@siswa.ukm.edu.my>
2020-08-03 18:47:31 +0800

    Added comments

--- concurrent_futures.py
+++ concurrent_futures.py
@@ -1,7 +1,7 @@
 # The ProcessPoolExecutor class is an Executor subclass that uses a pool of processes to execute calls asynchronously.
 # ProcessPoolExecutor uses the multiprocessing module, which allows it to side-step the Global Interpreter Lock
 # but also means that only picklable objects can be executed and returned.
-
+# ThreadPoolExecutor class implements Threading instead of Multiprocessing
 import concurrent.futures
 import time
 
@@ -13,7 +13,7 @@ def do_a_task(seconds) -> str:
     time.sleep(seconds)
     return f'Done sleeping for {seconds} second(s)'
 
-
+# THE WITH FUNCTION acts as a context manager. Auto joins all the processes
 # '''submit many processes'''
 # with concurrent.futures.ProcessPoolExecutor() as executor:
 #     results = [executor.submit(do_a_task, 2) for i in range(10)]
@@ -23,13 +23,13 @@ def do_a_task(seconds) -> str:
 
 
 '''submit many processes with varying times'''
-with concurrent.futures.ProcessPoolExecutor() as executor:
-    secs = [5, 4, 3, 2, 1]
-    # list(reversed(secs))
-    results = [executor.submit(do_a_task, sec) for sec in secs]
-    for f in concurrent.futures.as_completed(results):
-        print(f.result())
-        # returns the object
+# with concurrent.futures.ProcessPoolExecutor() as executor:
+#     secs = [5, 4, 3, 2, 1]
+#     # list(reversed(secs))
+#     results = [executor.submit(do_a_task, sec) for sec in secs]
+#     for f in concurrent.futures.as_completed(results):
+#         print(f.result())
+#         # returns the object
 
 '''map function maps the whole list to the function and simply return the result. Map returns result as the 
 processes were started (order) not as they were completed.'''
